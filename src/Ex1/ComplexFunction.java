5d5ecd49214e0b74ed54fc9fb8764e18919f0dd8
yanashayev <yana.shayev123@gmail.com>
2019-12-11 19:30:06 +0200

    update ComplexFunction

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -9,21 +9,49 @@ public class ComplexFunction implements complex_function {
 		ComplexFunction f= new ComplexFunction(s);
 		System.out.println(f.toString());
 		System.out.println(f.f(1));
+
+		Monom m = new Monom("2x");
+		Polynom p = new Polynom("2x^2");
+		ComplexFunction cf = new ComplexFunction(Operation.Plus,p,m);
+		System.out.println(cf.toString());
+
+		ComplexFunction r = new ComplexFunction(Operation.Times,cf,m);
+		System.out.println(r);
+
+//		ComplexFunction cc = new ComplexFunction(cf.copy());
+//		System.out.println(cc.toString());
+//		String s4="plus(-1.0x^4+2.4x^2+3.1,0.1x^5-1.2999999999999998x+5.0)";
+//		String s3="plus(div(1.0x+1.0,mul(mul(1.0x+3.0,1.0x-2.0),1.0x-4.0)),2.0)";
+//		String s0="div(plus(-1.0x^4+2.4x^2+3.1,0.1x^5-1.2999999999999998x+5.0),-1.0x^4+2.4x^2+3.1)";
+//		String s5="-1.0x^4+2.4x^2+3.1";
+//		String s6="0.1x^5-1.2999999999999998x+5.0";
+//		String s7="max(max(max(max(plus(-1.0x^4+2.4x^2+3.1,0.1x^5-1.2999999999999998x+5.0),plus(div(1.0x+1.0,mul(mul(1.0x+3.0,1.0x-2.0),1.0x-4.0)),2.0)),div(plus(-1.0x^4+2.4x^2+3.1,0.1x^5-1.2999999999999998x+5.0),-1.0x^4+2.4x^2+3.1)),-1.0x^4+2.4x^2+3.1),0.1x^5-1.2999999999999998x+5.0)";
+//		String s8="min(min(min(min(plus(-1.0x^4+2.4x^2+3.1,0.1x^5-1.2999999999999998x+5.0),plus(div(1.0x+1.0,mul(mul(1.0x+3.0,1.0x-2.0),1.0x-4.0)),2.0)),div(plus(-1.0x^4+2.4x^2+3.1,0.1x^5-1.2999999999999998x+5.0),-1.0x^4+2.4x^2+3.1)),-1.0x^4+2.4x^2+3.1),0.1x^5-1.2999999999999998x+5.0)";
+//ComplexFunction f4= new ComplexFunction(s4);
+//		System.out.println(f4.toString());
+
 	}
 
-	private Operation op; //HAVE TO BE STRING
+	private Operation op= Operation.None; //HAVE TO BE STRING
 	private function left, right=null;
 	public ComplexFunction() {
-		op = Operation.None;
-		left = new Polynom();
-		right = new Polynom();
+
 	}
-	public ComplexFunction(String s){
-		ComplexFunction c =(ComplexFunction) initFromString(s);
-		this.op=c.op;
-		this.left=c.left;
-		this.right=c.right;
+	public ComplexFunction(String s) {
+		function f=initFromString(s);
+		if (f instanceof ComplexFunction) {
+			ComplexFunction c = (ComplexFunction) f;
+			this.op = c.op;
+			this.left = c.left;
+			this.right = c.right;
+		}
+		else{
+			this.op = Operation.None;
+			this.left = f;
+
+		}
 	}
+
 	public ComplexFunction(String op, function left, function right) {
 		this.op=checkWhichOperation(op);
 		this.left=left;
@@ -32,13 +60,13 @@ public class ComplexFunction implements complex_function {
 	}
 	public ComplexFunction(Operation op, function left, function right) {
 		this.op=op;
-		this.left=left;
-		this.right=right;
+		this.left=left.copy();
+		this.right=right.copy();
 
 	}
 
 	public ComplexFunction(function left) {
-		this.left=left;
+		this.left=left.copy();
 	}
 
 	public ComplexFunction(function left,function right) {
@@ -56,18 +84,32 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public String toString() {
-		return this.op+"("+this.left+","+this.right+")";
-
+		if(this.right==null){
+			return ""+this.left;
+		}
+		switch (getOp()) {
+			case Times: {
+				return "Mul(" + this.left + "," + this.right + ")";
+			}
+			default:
+				return this.op + "(" + this.left + "," + this.right + ")";
+		}
 	}
 
 	@Override
 	public double f(double x) {
 
-		switch (op){
+		switch (this.op){
 			case Max:return Math.max(left.f(x),right.f(x));
 			case Min:return Math.min(left.f(x),right.f(x));
 			case Comp:return left.f(right.f(x));
-			case None:throw new RuntimeException("None action");
+			case None:{
+				if (right == null) {
+					return left.f(x);
+				}else {
+					throw new RuntimeException("There is no Operation to calculate between the functions");
+				}
+			}
 			case Plus:return left.f(x)+right.f(x);
 			case Divid:{
 				if(right.f(x)==0){ throw new RuntimeException("divide by 0");}
@@ -76,12 +118,15 @@ public class ComplexFunction implements complex_function {
 				}
 			}
 			case Times:return left.f(x)*right.f(x);
-			case Error:
-			default:
+
+			case Error: {
 				throw new RuntimeException("ERROR OP IS EROOR");
+			}
+			default :
+				return this.f(x);
 		}
 	}
-	
+
 	public String stringOp (String s) {
 		String t="";
 		int i=0;
@@ -114,8 +159,7 @@ public class ComplexFunction implements complex_function {
 		return s.substring(start,end);
 
 
-	}
-	public String stringRight(String s) {
+	}public String stringRight(String s) {
 		int counter=0;
 		int start=0;
 		int end=0;
@@ -172,12 +216,14 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public function initFromString(String s) {
-		try{
+		s=s.replaceAll(" ", "");
+
+
+			try{
 			Polynom p = new Polynom(s);
 			return p;
-		}catch (Exception e){
-
-		}
+		}catch (Exception e) {
+			}
 		ComplexFunction f= new ComplexFunction();
 
 
@@ -218,13 +264,18 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public function copy() {
-		 return initFromString(this.toString());
-
+		 //return initFromString(this.toString());
+		if(right() == null) {
+			return new ComplexFunction(left.copy());
+		}else {
+			ComplexFunction copy = new ComplexFunction(this.op,left().copy(),right().copy());
+			return copy;
+		}
 	}
 
 	@Override
 	public void plus(function f1) {
-		ComplexFunction temp= (ComplexFunction) this.copy();
+		function temp = this.copy();
 		this.op = Operation.Plus;
 		this.right=f1;
 		this.left=temp;
@@ -233,16 +284,16 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public void mul(function f1) { //MULTIPLY
-		ComplexFunction temp= (ComplexFunction) this.copy();
+		function temp=this.copy();
 		this.op = Operation.Times;
 		this.right=f1;
-		this.left=temp;
+		this.left=temp.copy();
 
 	}
 
 	@Override
 	public void div(function f1) {
-		ComplexFunction temp= (ComplexFunction) this.copy();
+		function temp= this.copy();
 		this.op = Operation.Divid;
 		this.right=f1;
 		this.left=temp;
@@ -251,7 +302,7 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public void max(function f1) {
-		ComplexFunction temp= (ComplexFunction) this.copy();
+		function temp= this.copy();
 		this.op = Operation.Max;
 		this.right=f1;
 		this.left=temp;
